113998059d3e6b4b286ce60c2aaace9565f8c1b3
Sharla Gelfand <sharla.gelfand@gmail.com>
2017-02-12 19:26:15 -0800

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Weekly horoscopes from Chani Nicholas
+#'
+#' Returns weekly horoscopes from \url{http://chaninicholas.com}. Includes data
+#' for 2015-2017, all weeks that horoscopes were written for (start dates
+#' between January 5, 2015 and February 13, 2017).
+#'
+#' @format A data frame with 1248 rows and 4 variables:
+#' \describe{
+#'   \item{start_date}{the start date that this weekly horoscope is for}
+#'   \item{zodiacsign}{the zodiac sign this horoscope is for}
+#'   \item{horoscope}{the horoscope itself}
+#'   \item{url}{the URL that the horoscope is pulled from}
+#' }
+#' @source \url{http://chaninicholas.com}
+"horoscopes"
---
+++ data-raw/horoscopes.R
@@ -0,0 +1,116 @@
+library(rvest)
+library(stringr)
+library(dplyr)
+library(tidytext)
+library(lubridate)
+library(httr)
+library(forcats)
+library(purrr)
+library(tidyr)
+
+# Check which URLs exist for all year/month combinations
+horoscopes <- crossing(year = 2015:2017,
+                       month = c("01", "02", "03", "04", "05", "06",
+                                 "07", "08", "09", "10", "11", "12")) %>%
+  mutate(date_url = map2_chr(year, month,
+                             ~ str_c("http://chaninicholas.com/", .x, "/", .y)),
+         url_exists = map_lgl(date_url,
+                              ~ (status_code(GET(.x)) >= 300) == FALSE)) %>%
+  filter(url_exists)
+
+# Get page 2 of horoscopes for year/month combinations that exist (if it exists too!)
+horoscopes <- horoscopes %>%
+  bind_rows(horoscopes %>%
+              mutate(date_url = str_c(date_url, "/page/2"),
+                     url_exists = NA)) %>%
+  mutate(url_exists = if_else(is.na(url_exists),
+                              map_lgl(date_url,
+                                      ~ (status_code(GET(.x)) >= 300) == FALSE),
+                              url_exists)) %>%
+  filter(url_exists)
+
+# Extract horoscope links from each date link
+horoscopes <- horoscopes %>%
+  mutate(url = pmap(list(date_url, year, month),
+                    function(date_url, year, month)
+                      date_url %>%
+                      read_html() %>%
+                      html_nodes("a") %>%
+                      html_attr("href") %>% # extract the URLs
+                      str_subset('horoscopes') %>% # only horoscope links
+                      str_subset(str_c(year, "/", month)) %>% # for that year/month only
+                      unique())) %>%
+  unnest(url) %>%
+  select(-date_url) %>%
+  unique()
+
+# Extract start date for each horoscope
+horoscopes <- horoscopes %>%
+  mutate(day = map_dbl(url,
+                       ~ str_match(str_match(.x, "horoscopes(.*?)/")[,2], "[0-9]+") %>%
+                         as.numeric()),
+         start_date = ymd(str_c(year, "/", month, "/", day)))
+
+# Fill in missing dates
+horoscopes <- horoscopes %>%
+  mutate(date_manual = case_when(.$url == "http://chaninicholas.com/2015/01/new-moon-aquariusmercury-retrograde-horoscopes/" ~ ymd("2015-01-19"),
+                                 .$url == "http://chaninicholas.com/2015/02/new-moon-aquarius-horoscopes/" ~ ymd("2015-02-16"),
+                                 .$url == "http://chaninicholas.com/2015/12/horoscopes-for-the-winter-solstice-and-the-full-moon-in-cancer/" ~ ymd("2015-12-21")),
+         start_date = if_else(is.na(start_date), date_manual, start_date)) %>%
+  select(-day, -date_manual)
+
+
+# Extract horoscope text, split into paragraphs
+horoscopes <- horoscopes %>%
+  mutate(text = map_chr(url,
+                        ~ .x %>%
+                          read_html() %>%
+                          html_nodes('.entry-content') %>%
+                          html_text() %>%
+                          iconv(to = 'UTF-8') %>%
+                          str_replace_all(c("[\r]" = " ", "[“‘’”]" = "'"))),
+         text_split = map(text,
+                          ~ .x %>%
+                            strsplit('\n') %>%
+                            unlist() %>%
+                            str_trim()),
+         text_split = map(text_split,
+                          ~ .x[.x != ""]))
+
+# Remove duplicate elements within each horoscope (ads, etc)
+horoscopes <- horoscopes %>%
+  mutate(text_split = map(text_split,
+                          ~ .x[!(duplicated(.x)) & !(duplicated(.x, fromLast = TRUE))]),
+         text_length = map_dbl(text_split, ~length(.x)))
+
+# Get the horoscope that corresponds to each sign
+horoscopes <- horoscopes %>%
+  mutate(join = TRUE) %>%
+  left_join(tibble(zodiacsign = c("Aries", "Taurus", "Gemini" ,"Cancer",
+                            "Leo", "Virgo", "Libra", "Scorpio",
+                            "Sagittarius", "Capricorn", "Aquarius", "Pisces"),
+                   join = TRUE),
+            by = "join") %>%
+  group_by(start_date, zodiacsign) %>%
+  mutate(start_of_sign = map2(text_split, zodiacsign,
+                              ~ which(str_detect(.x, str_c("^", .y, ".*Rising")) |
+                                        str_detect(.x, str_c("^", .y, " &")) |
+                                        .x == .y)),
+         start_of_sign = ifelse(length(unlist(start_of_sign)) != 0,
+                                unlist(start_of_sign) + 1,
+                                NA)) %>%
+  ungroup() %>%
+  mutate(end_of_sign = ifelse(zodiacsign != 'Pisces',
+                              lead(start_of_sign) - 2,
+                              text_length),
+         horoscope = pmap_chr(list(text_split, start_of_sign, end_of_sign),
+                              function(text_split, start_of_sign, end_of_sign) 
+                                str_c(text_split[start_of_sign:end_of_sign], collapse = " ")))
+
+# Remove exta variables
+horoscopes <- horoscopes %>%
+  select(start_date, zodiacsign, horoscope, url)
+
+# Add data files to package
+
+devtools::use_data(horoscopes, overwrite = TRUE)
